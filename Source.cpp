6ae7c02d2959a336e1e621f447c3675d0201f77d
VladGBRL <86017579+VladGBRL@users.noreply.github.com>
2024-11-05 14:14:38 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
 
-using namespace std;
+void createProcess(int waitSeconds) {
+    pid_t pid = fork();
+
+    if (pid == 0) { 
+        sleep(waitSeconds); 
+        std::cout << "Process " << waitSeconds << " seconds done.\n";
+        _exit(0);
+    }
+}
 
 int main() {
-	cout << "Hello world!";
-	return 0;
+
+    createProcess(1);
+    createProcess(2);
+    createProcess(3);
+
+    while (wait(nullptr) > 0); 
+
+    std::cout << "All processes completed.\n";
+    return 0;
 }
